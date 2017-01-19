4a38ab3ed0e6f6264ada432225c64bdecdf659a3
Nathan Rathbun <naterathbun@gmail.com>
2017-01-19 15:55:58 -0500

    Built board class, draws grid graphics

---
+++ TictacToe/TictacToe/Classes/Board.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TictacToe.Classes
+{
+    class Board
+    {
+        public List<char> boardTally = new List<char>();
+        public char player1Symbol;
+        public char player2Symbol;
+
+        public Board()                                  // blank the board
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                boardTally.Add(' ');
+            }
+        }
+
+        public bool addPlayer1Symbol(int location)      // takes location, if available set board[location] to player symbol, if not return false
+        {
+            if ((location <= 9) && (location >= 0) && (boardTally[location] != ' '))
+            {
+                boardTally[location] = player1Symbol;
+                return true;
+            }
+            return false;
+        }
+
+        public void printBoard()
+        {
+            Console.WriteLine($"\n  __________________________");
+            Console.WriteLine($"/|                          |\\");
+            Console.WriteLine($"||         |     |          ||");
+            Console.WriteLine($"||      {boardTally[0]}  |  {boardTally[1]}  |  {boardTally[2]}       ||");
+            Console.WriteLine($"||    _____|_____|_____     ||");
+            Console.WriteLine($"||         |     |          ||");
+            Console.WriteLine($"||      {boardTally[3]}  |  {boardTally[4]}  |  {boardTally[5]}       ||");
+            Console.WriteLine($"||    _____|_____|_____     ||");
+            Console.WriteLine($"||         |     |          ||");
+            Console.WriteLine($"||      {boardTally[0]}  |  {boardTally[1]}  |  {boardTally[2]}       ||");
+            Console.WriteLine($"||         |     |          ||");
+            Console.WriteLine($"\\|__________________________|/");
+        }
+    }
+}
--- TictacToe/TictacToe/Program.cs
+++ TictacToe/TictacToe/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using TictacToe.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,22 @@ namespace TictacToe
     {
         static void Main(string[] args)
         {
+            Board board = new Board();
+
+            Console.WriteLine("Enter Symbol for Player 1: ");
+            board.player1Symbol = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter Symbol for Player 2: ");
+            board.player2Symbol = Convert.ToChar(Console.ReadLine());
+
+
+            board.printBoard();
+
+            Console.WriteLine($"\n Player 1 Symbol: {board.player1Symbol}");
+            Console.WriteLine($"\n Player 2 Symbol: {board.player2Symbol}");
+
+            Console.ReadLine();
+
         }
     }
 }
